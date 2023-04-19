857711fc5d64cb4620790f06ac5e055300dd54b0
Felipe Sousa <felipe.fps09@hotmail.com>
2023-04-19 15:29:05 -0300

    Resolução de problemas

---
+++ src/application/Main.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        acharUmNumeroEmUmArrayList(sc);
+
+        sc.close();
+
+    }
+
+    public static void coresFavoritas(Scanner sc) {
+        ArrayList<String> cores = new ArrayList<>();
+
+        System.out.println("Digite 5 cores: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print((i + 1) + ": ");
+            cores.add(sc.nextLine());
+        }
+        System.out.println("Listar todas as cores: ");
+        cores.forEach(System.out::println);
+
+        Collections.sort(cores);
+        System.out.println("Ordenar as cores: ");
+        cores.forEach(System.out::println);
+
+    }
+
+    public static void acharUmNumeroEmUmArrayList(Scanner sc) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(5);
+        arrayList.add(1);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(9);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(10);
+        arrayList.add(6);
+
+        System.out.print("Digite o número que você deseja encontrar: ");
+        int num = sc.nextInt();
+
+        System.out.println((arrayList.contains(num)) ? "O número " + num + " está localizado na posição: " + arrayList.indexOf(num) : "O número " + num + " não foi encontrado!");
+    }
+
+
+}
---
+++ src/exerciseSet/Main.java
@@ -0,0 +1,53 @@
+package exerciseSet;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        setTemOuNaoNumDig(sc);
+
+        sc.close();
+    }
+
+    public static void dig10NumEmListSet(Scanner sc) {
+        Set<Integer> setCom10 = new HashSet<>();
+
+        Iterator<Integer> iteCom10 = setCom10.iterator();
+        int i = 0;
+        while (i < 10) {
+            System.out.print("Digite o " + (i + 1) + " valor: ");
+            setCom10.add(sc.nextInt());
+            i++;
+        }
+
+        System.out.println("Lista dados do Set: ");
+        while (iteCom10.hasNext()) {
+            System.out.println(setCom10);
+            iteCom10.next();
+        }
+    }
+
+    public static void setTemOuNaoNumDig(Scanner sc) {
+        Set<Integer> setList = new HashSet<>();
+        setList.add(2);
+        setList.add(5);
+        setList.add(1);
+        setList.add(3);
+        setList.add(4);
+        setList.add(9);
+        setList.add(7);
+        setList.add(8);
+        setList.add(10);
+        setList.add(6);
+
+        System.out.print("Digite o número que você deseja encontrar: ");
+        int num = sc.nextInt();
+
+        String result = (setList.contains(num)) ? "O número " + num + " foi encontrado " : "O número " + num + " não foi encontrado!";
+
+        System.out.println(result);
+    }
+
+}
